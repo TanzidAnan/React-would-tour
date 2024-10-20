4e95506676894680acf514d54e3bc1fc720f81f9
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-20 17:23:15 +0600

    visited data

--- src/Compuntent/Contry/Country.jsx
+++ src/Compuntent/Contry/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country ,hendleVisitedCountery}) => {
     // console.log(country)
     const {name,flags,area,population,cca3} =country;
 
@@ -9,7 +9,7 @@ const Country = ({country}) => {
     const hendleVisited =() =>{
         setVisited(!visited)
     }
-
+    console.log(hendleVisitedCountery)
     return (
         <div className={`country ${visited && 'visited'}`}>
             <h5>Name:{name?.common} </h5>
@@ -19,6 +19,7 @@ const Country = ({country}) => {
             <p>area: {area}</p>
             <p>population: {population}</p>
             <p><small>Code: {cca3}</small></p>
+            <button className='btn'>Mark visited</button>
             <button className='btn' onClick={hendleVisited}>{visited?'Visited':'Going'}</button>
             {visited ? 'I Have a visited': 'I one to visite'}
         </div>
--- src/Compuntent/Countries/Countries.jsx
+++ src/Compuntent/Countries/Countries.jsx
@@ -5,18 +5,33 @@ import './Countries.css'
 
 const Countries = () => {
     const [countris, setCountris] = useState([]);
+    const [visitedCountery, setvisitedCountery] =useState([])
     useEffect(() => {
         fetch(`https://restcountries.com/v3.1/all`)
             .then(res => res.json())
             .then(data => setCountris(data))
     }, []);
-    console.log(countris)
+    const hendleVisitedCountery = countery =>{
+        console.log(54545)
+        console.log(countery)
+    }
+     console.log(countris)
     return (
         <div>
             <h3>Countries: {countris.length}</h3>
+            <div>
+                <h3>visited Country</h3>
+                <ul>
+
+                </ul>
+            </div>
             <div className="country-conterner">
                 {
-                    countris.map(countris => <Country key={countris.cca2} country={countris}></Country>)
+                    countris.map(countris => <Country
+                         key={countris.cca2} 
+                         country={countris}
+                         hendleVisitedCountery={hendleVisitedCountery}
+                         ></Country>)
                 }
             </div>
         </div>
