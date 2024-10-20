726eb124433c041936c2bd836acf3797fe2c5058
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-20 10:13:19 +0600

    load data api

--- src/Compuntent/Countries/Countries.jsx
+++ src/Compuntent/Countries/Countries.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Country from "../Contry/Country";
 
 const Countries = () => {
     const [countris,setCountris] =useState([]);
     useEffect(() =>{
         fetch(`https://restcountries.com/v3.1/all`)
         .then(res => res.json())
-        .then(data => console.log(data))
-    },[])
+        .then(data => setCountris(data))
+    },[]);
+    console.log(countris)
     return (
         <div>
-            <h3>Countries</h3>
+            <h3>Countries: {countris.length}</h3>
+            {
+                countris.map(countris => <Country country={countris}></Country>)
+            }
         </div>
     );
 };
