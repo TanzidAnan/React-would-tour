33979497ea65ed0f074efae116a81bcd939dcd0c
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-20 13:07:26 +0600

    cca3 code data load

--- src/Compuntent/Contry/Country.jsx
+++ src/Compuntent/Contry/Country.jsx
@@ -1,13 +1,16 @@
 import './Country.css'
 const Country = ({country}) => {
     // console.log(country)
-    const {name,flags,area,population} =country;
+    const {name,flags,area,population,cca3} =country;
     return (
         <div className='country'>
             <h5>Name:{name?.common} </h5>
+            <div className='img-div'>
             <img src={flags.png} alt="" />
+            </div>
             <p>area: {area}</p>
             <p>population: {population}</p>
+            <p><small>Code: {cca3}</small></p>
         </div>
     );
 };
